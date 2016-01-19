09ad90b605951a5988b0b2e17993f94235aef8d7
Frederick Wurfer <Frederick Wurfer>
2016-01-19 18:11:27 +0100

    lambda expressions added

--- cpp/LambdaExpression/LambdaExpression/FilterableList.h
+++ cpp/LambdaExpression/LambdaExpression/FilterableList.h
@@ -4,19 +4,66 @@
 #include <algorithm>
 #include <functional>
 #include <vector>
+#include <time.h>
 using namespace std;
 
-class FilterableList{
+template <class T> class FilterableList{
 private:
-	vector<int> fList;
+	vector<T> fList;
 public:
 	
-	FilterableList();
-	~FilterableList();
-	void add(int e);
-	void remove(int e);
-	bool has(int e);
-	//FilterableList<T>filter(Func<T>, bool);
+	FilterableList(){
+		fList;
+	}
+	~FilterableList(){
+		fList.clear();
+	}
+	void add(T e){
+		fList.push_back(e);
+	}
+	void remove(T e){
+		fList.erase(std::remove(fList.begin(), fList.end(), e), fList.end());
+	}
+	bool has(T e){
+		bool res = false;
+		for each (T i in fList){
+			if (i == e){
+				res = true;
+			}
+		}
+		return res;
+	}
+	
+	string randomString(){
+		//srand(time(NULL));
+		string rString = "";
+
+		int wLength = (rand() % 10) + 1;
+		for (int i = 0; i < wLength; i++){
+			char randomChar = (rand() % 25) + 97;
+			rString += randomChar;
+		}
+		return rString;
+	}
+
+	float randomFloat(){
+		float rFloat;
+		rFloat = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/100.0));
+		rFloat = rFloat - 50;
+		return rFloat;
+	}
+	void filter(function<bool(T)> func){
+		vector <T> results;
+		for (int i = 0; i < fList.size(); i++){
+			if (func(fList[i])){
+				cout << "True: " << fList[i] << endl;
+				results.push_back(fList[i]);
+			}
+			else {
+				cout << "False: " << fList[i] << endl;
+			}
+		}
+	}
 };
 
 #endif  
--- cpp/LambdaExpression/LambdaExpression/main.cpp
+++ cpp/LambdaExpression/LambdaExpression/main.cpp
@@ -1,17 +1,39 @@
 #include "FilterableList.h"
+#include <string>
 #include<iostream>
 using namespace std;
 
 int main(){
-	FilterableList  intList;
+	FilterableList<int>  intList;
 	intList.add(1);
 	intList.add(5);
 	intList.add(3);
 	intList.add(2);
 
-	cout << intList.has(1) << endl;
-	intList.remove(1);
-	cout << intList.has(1) << endl;
+	//creating a new String-List
+	FilterableList<string> stringList;
+	//adding random Strings
+	stringList.add(stringList.randomString());
+	stringList.add(stringList.randomString());
+	stringList.add(stringList.randomString());
+	
+	//creating a new Float-List
+	FilterableList<float> floatList;
+	//adding random Floats
+	floatList.add(floatList.randomFloat());
+	floatList.add(floatList.randomFloat());
+	floatList.add(floatList.randomFloat());
+
+	//String-Filtering
+	stringList.filter([](string s) -> bool {return s.length() > 2; });
+	stringList.filter([](string s) {return s.find('s'); });
+
+	//Float-Filtering
+	floatList.filter([](float f) -> bool {return f >= 5; });
+	floatList.filter([](float f) -> bool {return f < 0; });
+	//todo: rundung fixen!!!
+	floatList.filter([](float f) -> bool {f = f + 0.5; std::cout<<f<<endl; return int(f) % 2; });
+
 	system("pause");
 	return 0;
 }
